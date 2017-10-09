624581e42392baa266115cc9f3f8820bd68451f6
MathiasV54 <vmathias.fr@gmail.com>
2017-10-09 15:01:10 +0200

    Adaptation of the boilerplate of awurth for our own needs.

--- app/settings.php
+++ app/settings.php
@@ -9,7 +9,8 @@ return [
         'view' => [
             'templates_path' => dirname(__DIR__) . '/src/App/Resources/views',
             'twig' => [
-                'cache' => dirname(__DIR__) . '/var/cache/twig',
+//                'cache' => dirname(__DIR__) . '/var/cache/twig',
+                'cache' => false,
             ]
         ],
 
