ccecee17be9adb3b9e4126b295f70437e25f4e9a
LouisCuny <louis-cuny@orange.fr>
2017-10-12 16:40:13 +0200

    Here it comes, the gallery view with the firsts filters

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -2,3 +2,5 @@
 
 $app->get('/', 'app.controller:home')->setName('home');
 $app->get('/profile', 'app.controller:profile')->setName('profile');
+$app->get('/gallery', 'app.controller:gallery')->setName('gallery');
+
