f51b1bcae4243741e43b2bc791ed338b4dc65e6d
Allan Demarbre <demarbreallan@gmail.com>
2017-10-12 16:23:38 +0200

    Modifying the product route. A valid Product ID is now required for the route to be valid. Product details are also now shown in the page.

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -2,4 +2,4 @@
 
 $app->get('/', 'app.controller:home')->setName('home');
 $app->get('/profile', 'app.controller:profile')->setName('profile');
-$app->get('/product', 'app.controller:product');
+$app->get('/product/{id}', 'app.controller:product');
