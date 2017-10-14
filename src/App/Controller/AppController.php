eebd716fed7d44c11a240f3319ffcc8bacda7141
LouisCuny <louis-cuny@orange.fr>
2017-10-14 11:36:01 +0200

    Semantic to bootstrap on error views, change a few little mistakes

--- src/App/Controller/AppController.php
+++ src/App/Controller/AppController.php
@@ -19,16 +19,5 @@ class AppController extends Controller
         } else if ($user = $this->auth->getUser()) {
             return $this->view->render($response, 'App/profile.twig');
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    }
-
-    public function gallery(Request $request, Response $response)
-    {
-        return $this->view->render($response, 'App/gallery.twig');
->>>>>>> c31f6b62468ff7a6173f55cb2dd50e218eed8a06
-=======
->>>>>>> 6e2ac13d74e88a748a974e36fdbbc30eb30128ac
     }
 }
--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -1,13 +1,4 @@
 <?php
 
 $app->get('/', 'app.controller:home')->setName('home');
-<<<<<<< HEAD
 $app->get('/profile', 'app.controller:profile')->setName('profile');
-<<<<<<< HEAD
-=======
-$app->get('/gallery', 'app.controller:gallery')->setName('gallery');
-
->>>>>>> c31f6b62468ff7a6173f55cb2dd50e218eed8a06
-=======
-$app->get('/profile', 'app.controller:profile')->setName('profile');
->>>>>>> 6e2ac13d74e88a748a974e36fdbbc30eb30128ac
