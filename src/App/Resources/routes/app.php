3fb76f71c8b421b14d25a8cb2042729813b8d9a1
Allan Demarbre <demarbreallan@gmail.com>
2017-10-19 16:35:27 +0200

    The real price is now displayed. The "product" page's been renamed to "detail" page. The template has also been applied to it. The basic search bar functionnalities is working. You can access a product's detail via this page.

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -4,3 +4,4 @@ $app->get('/', 'app.controller:home')->setName('home');
 $app->get('/profile', 'app.controller:profile')->setName('profile');
 $app->get('/detail/{id}', 'app.controller:detail');
 $app->get('/search', 'app.controller:search')->setname('search');
+$app->post('/search', 'app.controller:search')->setname('search');
