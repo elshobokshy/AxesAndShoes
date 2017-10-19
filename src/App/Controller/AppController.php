012dc8592a51914510c60fdd6c7b03f4d8260eac
Allan Demarbre <demarbreallan@gmail.com>
2017-10-19 14:08:12 +0200

    commit before rebase

--- src/App/Controller/AppController.php
+++ src/App/Controller/AppController.php
@@ -161,4 +161,21 @@ class AppController extends Controller
         return $this->view->render($response, 'App/add.twig', $data);
     }
     
+    public function search(Request $request, Response $response)
+    {
+        $searching = $request->getParam('search');
+        if($searching)
+        {
+            $products = $this->container->db->table('product')->where('title',  'like',  '%' . $searching . '%')->get();      
+            $data = ["products" => []];
+            
+            foreach ($products as $p)
+            {   
+                print_r($p->title);
+                
+            }
+        }
+
+        return $this->view->render($response, 'App/search.twig');
+    }
 }
