a05df651fb50e307d059dac23205b0fcd1d76d8c
Allan Demarbre <demarbreallan@gmail.com>
2017-10-19 14:08:12 +0200

    commit before rebase

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -4,9 +4,12 @@ $app->get('/', 'app.controller:home')->setName('home');
 $app->get('/profile', 'app.controller:profile')->setName('profile');
 <<<<<<< 639f634667805bd4c70962adab8cc88a5f2caf77
 $app->get('/product/{id}', 'app.controller:product');
+<<<<<<< 4a7a42d1efcddd7ff1cd18b2f7a8fbdb95e6714b
 $app->get('/add', 'app.controller:add')->setName('add');
 $app->post('/add', 'app.controller:add');
 =======
 $app->get('/detail/{id}', 'app.controller:detail');
 >>>>>>> Renaming the "product" page "detail"
+=======
+>>>>>>> commit before rebase
 $app->get('/search', 'app.controller:search')->setname('search');
