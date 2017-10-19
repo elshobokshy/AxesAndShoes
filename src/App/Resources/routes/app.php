639f634667805bd4c70962adab8cc88a5f2caf77
Allan Demarbre <demarbreallan@gmail.com>
2017-10-19 14:08:12 +0200

    commit before rebase

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -5,3 +5,4 @@ $app->get('/profile', 'app.controller:profile')->setName('profile');
 $app->get('/product/{id}', 'app.controller:product');
 $app->get('/add', 'app.controller:add')->setName('add');
 $app->post('/add', 'app.controller:add');
+$app->get('/search', 'app.controller:search')->setname('search');
