cb63979dd8071c1336186f14684293738d8b05f9
Islam Elshobokshy <islam20088@hotmail.com>
2017-10-20 22:06:11 +0200

    Created 'adding to rent' controller

--- src/App/Controller/AppController.php
+++ src/App/Controller/AppController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use Respect\Validation\Validator as V;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use App\Model\Color;
+use App\Model\Material;
+use App\Model\Product;
 
 class AppController extends Controller
 {
@@ -54,4 +58,108 @@ class AppController extends Controller
 
         return $this->view->render($response, 'App/product.twig', $data);
     }
+
+    public function add(Request $request, Response $response)
+    {   
+        
+        if ($request->isPost()) {
+            $title = $request->getParam('title');
+            $description = $request->getParam('description');
+            $size = $request->getParam('size');
+            $waterproof = $request->getParam('waterproof'); // 'Yes' or 'No'
+            $price = $request->getParam('price');
+            $material = $request->getParam('material');
+            $color = $request->getParam('color');
+
+            $imgs = array();
+            $cnt = count($_FILES['image']);
+
+            $allowed =  array('gif','png' ,'jpg', 'jpeg');
+            $noErrors = '1';
+            
+            // Loop to make sure every item uploaded is an image with an extension in $allowed
+            for($i = 0 ; $i < $cnt ; $i++) {
+                if(isset($_FILES['image']['name'][$i])) {
+                    $filename = $_FILES['image']['name'][$i];      
+                    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                    if( !in_array($ext,$allowed) ) {
+                        $noErrors = '0';
+                    }
+                }
+            }
+
+            if( $noErrors = '0' ) {
+                $this->validator->addError('image', 'Image needs to only have these extensions : gif, png, jpg, jpeg.');
+            }
+            
+            
+            $this->validator->request($request, [
+                'title' => V::notEmpty()->alpha()->length(3, 25),
+                'description' => V::notEmpty()->length(5, 1000)->alpha(),
+                'size' => V::notEmpty()->noWhitespace()->Numeric()->positive()->between(1, 255),
+                'price' => V::notEmpty()->noWhitespace()->Numeric()->positive()->between(1, 999),
+                'waterproof' => V::notEmpty()->noWhitespace()->length(1, 3),
+                'material' => V::notEmpty()->length(3, 20),
+                'color' => V::notEmpty()->length(3, 20),
+            ]);
+
+            // img folder
+            $img = $_SERVER['DOCUMENT_ROOT'] . '/img/';
+            $conc = '';
+
+            // Changing the name of each file uploaded and uploading it in img folder
+            if( $noErrors = '1' ) {
+                for($i = 0 ; $i < $cnt ; $i++) {
+                    $name = uniqid('img-'.date('Ymd').'-');
+                    if(isset($_FILES['image']['tmp_name'][$i]) ) {
+                        if (move_uploaded_file($_FILES['image']['tmp_name'][$i], $img . $name) === true) {
+                            $conc = $conc . '"url":"'. $name . '",';
+                        }
+                    }
+                }
+                $jsonImgs = '{"img":[{'. $conc .'}]}';
+            }
+
+            if (Product::where('title', '=', $title)->exists()) {
+                $this->validator->addError('title', 'Title already exists. Please use a unique title.');
+            }
+
+            if ($this->validator->isValid()) {
+                $product = new Product();
+
+                $credentials = [
+                    'title' => $title,
+                    'description' => $description,
+                    'size' => $password,
+                    'waterproof' => $waterproof,
+                    'price' => $price,
+                    'material' => $material,
+                    'color' => $color,
+                    'image' => $files
+                ];
+
+                $product->title = $title;
+                $product->description = $description;
+                $product->size = $size;
+                $product->waterproof = $waterproof;
+                $product->price = $price;
+                $product->color = $color;
+                $product->material = $material;
+                $product->image = $jsonImgs;
+                $product->save();
+
+                $this->flash('success', 'Your shoes has been put to rent to the public.');
+
+                return $this->redirect($response, 'home');
+            }
+        }
+
+        $data = [
+            "colors" => Color::all(),
+            "materials" => Material::all(),
+        ];
+
+        return $this->view->render($response, 'App/add.twig', $data);
+    }
+    
 }
